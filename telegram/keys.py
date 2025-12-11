d135ee7c2be351a63e880b5160f366d4f17b4157
kenijan <130461862+kenijan@users.noreply.github.com>
2025-12-11 09:14:07 +0530

    Fix user_keys API and key values formatting

--- telegram/keys.py
+++ telegram/keys.py
@@ -1,24 +1,24 @@
 
 # user_keys = {
 #     "kenu": {
-#         "api": "4SSEtmVyKvFYStMPNmXnIUx47wtOFyEZRQ7hjABKpD1cNVNCoqPrENPvA6i1BoIw",
-#         "key": "4jGhDKiac1LuGS4Fzfv0SITvV05FvpI4ZMlbW1nGNZ9rScLeG2oTiJTHWrCWWNki"
+#         "api": "4SSEtmVyKvFYStMPNmXnIUx47KpD1cNVNCoqPrENPvA6i1BoIw",
+#         "key": "4jGhDKiac1LuGS4Fzfv0SITvV05ScLeG2oTiJTHWrCWWNki"
 #     },
 #     "sajai": {
-#         "api": "UL9IqkVX9YLc0JzTN7gRwkKiHhFOkKlWj3WhOiSsBpE6Hd0MUnwpYHAcH3DmM7D3",
-#         "key": "Z8dm0rf44VtS578j81GcaQrIYKnpCFC9E7kwLh4O0kV0Yxxi8yOfE7iUCppvEEfQ"
+#         "api": "UL9IqkVX9YLc0JzTN7gRwkKiHsBpE6Hd0MUnwpYHAcH3DmM7D3",
+#         "key": "Z8dm0rf44VtS578j81GcaQrIYKnpCFC9E7kwLUCppvEEfQ"
 #     },
 #     "jeni": {
-#         "api": "cWRvSY9TMGMYplgADZZzcxG20Tvi60Lp8o9CE4qyGfsIUQo7JKOIqWmHbUDIjodX",
-#         "key": "Jpb25uxfAMXw6xH2YttrS53rqVPUGLKtmSiRCj4Ao2B1aBUPu8hX50EEM4ymDMkZ"
+#         "api": "cWRvSY9TMGMYplgADZZzcxG20Tvi60Lp87JKOIqWmHbUDIjodX",
+#         "key": "Jpb25uxfAMXw6xH2YttrS53rqVPUGAo2B1aBUPu8hX50EEM4ymDMkZ"
 #     },
 #     "parthath": {
-#         "api": "WdezMlSXFPpRKeQmugfomPbmTWIHAOqrXCfohJivvylFXHnWfI8iDHhL7DbWi2wR",
-#         "key": "68JoQ8vmt7RuyqvW2iD4TAo9YTo5iXkjjuR5hdfz1SDqmOlGp5TaOUBtF6jgjw9R"
+#         "api": "WdezMlSXFPpRKeQmugfomPbmTWXCfohJivvylFXHnWfI8iDHhL7DbWi2wR",
+#         "key": "68JoQ8vmt7RuyqvW2iD4TAo9YToDqmOlGp5TaOUBtF6jgjw9R"
 #     },
 #     "kamal": {
-#         "api": "P9J504AkEF6LYB6rnKDJEoIMtJLs2XV31xGygPLpdzG6Y6vJ15ZjAtvbnjWee1ah",
-#         "key": "U3X7fC5RifclBhnV6aPRnIhdjTLvJLVdu3kwH4GPMyATNWlkDLmzlqGLGsdwMIww"
+#         "api": "P9J504AkEF6LYB6rnKDJEoIMtJLs2XV31xGygPvbnjWee1ah",
+#         "key": "U3X7fC5RifclBhnV6aPRnIhdjTLvJLVdu3kwH4GsdwMIww"
 #     }
 # }
 user_keys = {
